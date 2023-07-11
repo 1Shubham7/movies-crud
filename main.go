3041459a465bfee86a845cea802790e912e57901
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-11 23:25:08 +0530

    movie and director struct

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	
+}
+
+func main() {
+
+}
