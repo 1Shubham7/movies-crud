88913461f557539b6d41b52c1f08769ac5bd1550
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-12 13:50:15 +0530

    get movie done

--- main.go
+++ main.go
@@ -53,4 +53,16 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
 			break
 		}
 	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _,item := range movies {
+		if item.ID := params["id"]
+		json.NewEncoder(w).Encode(item) // simply return it
+		return
+	}
+
 }
