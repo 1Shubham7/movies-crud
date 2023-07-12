e3c8fe47c624c713a7ac3b2f108dcf3aa69caa6d
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-12 12:37:58 +0530

    movies handlefunc and server at port 6969

--- main.go
+++ main.go
@@ -18,9 +18,20 @@ type Movie struct {
 }
 
 type Director struct {
-	
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
 }
 
-func main() {
+var movies []Movie
 
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting the server at port 3000\n")
+	log.Fatal(http.ListenAndServe(":3000",r))
 }
